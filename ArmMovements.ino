1fb286c1efd0c628bdb57ad5c5c9f0d0471140a8
Jason Hu <s1928877@ed.ac.uk>
2022-04-04 11:46:19 +0100

    arduino communication and actions

---
+++ ArmMovements.ino
@@ -0,0 +1,125 @@
+#include <Braccio.h>
+#include <Servo.h>
+
+Servo base;
+Servo shoulder;
+Servo elbow;
+Servo wrist_rot;
+Servo wrist_ver;
+Servo gripper;
+
+enum actions {
+  TOP_LEFT_UPPER = 1,
+  TOP_LEFT_LOWER,
+  TOP_RIGHT_UPPER,
+  TOP_RIGHT_LOWER,
+  BOTTOM_LEFT_UPPER,
+  BOTTOM_LEFT_LOWER,
+  BOTTOM_RIGHT_UPPER,
+  BOTTOM_RIGHT_LOWER,
+  DROP_OFF_01,
+  DROP_OFF_34,
+  DROP_OFF_2,
+  DROP_OFF_5
+}
+
+// tlu = top left upper, brl = bottom right lower
+void pickup_action_tlu() {
+
+}
+
+void pickup_action_tll() {
+
+}
+
+void pickup_action_tru() {
+
+}
+
+void pickup_action_trl() {
+
+}
+
+void pickup_action_blu() {
+
+}
+
+void pickup_action_bll() {
+
+}
+
+void pickup_action_bru() {
+
+}
+
+void pickup_action_brl() {
+
+}
+
+
+void drop_off_action_01() {
+  // TODO, not last row, rhs, command should be '9'
+
+}
+
+void drop_off_action_34() {
+  // TODO, not last row, lhs, command should be '10'
+
+}
+
+void drop_off_action_2() {
+  // TODO, last row, rhs, command should be '11'
+
+}
+
+void drop_off_action_5() {
+  // TODO, last row, lhs, command should be '12'
+
+}
+
+
+void setup() {
+  //Initialization functions and set up the initial position for Braccio
+  //All the servo motors will be positioned in the "safety" position:
+  //Base (M1):90 degrees
+  //Shoulder (M2): 45 degrees
+  //Elbow (M3): 180 degrees
+  //Wrist vertical (M4): 180 degrees
+  //Wrist rotation (M5): 90 degrees
+  //gripper (M6): 10 degrees
+
+pinMode(12, OUTPUT);    //you need to set HIGH the pin 12
+digitalWrite(12, HIGH);
+Braccio.begin(SOFT_START_DISABLED);
+//Braccio.begin(0);
+Serial.begin(9600);
+
+}
+
+void loop() {
+   /*
+   Step Delay: a milliseconds delay between the movement of each servo.  Allowed values from 10 to 30 msec.
+   M1=base degrees. Allowed values from 0 to 180 degrees
+   M2=shoulder degrees. Allowed values from 15 to 165 degrees
+   M3=elbow degrees. Allowed values from 0 to 180 degrees
+   M4=wrist vertical degrees. Allowed values from 0 to 180 degrees
+   M5=wrist rotation degrees. Allowed values from 0 to 180 degrees
+   M6=gripper degrees. Allowed values from 10 to 73 degrees. 10: the toungue is open, 73: the gripper is closed.
+  */
+
+  // (step delay, M1, M2, M3, M4, M5, M6);
+  if (Serial.available() > 0) {
+    actions command = (actions) (unsigned int) Serial.read();
+    switch (command) {
+      case TOP_LEFT_UPPER: pickup_action_tlu(); Serial.write((unsigned int)command); break;
+      case TOP_LEFT_LOWER: pickup_action_tll(); Serial.write((unsigned int)command); break;
+      case TOP_RIGHT_UPPER: pickup_action_tru(); Serial.write((unsigned int)command); break;
+      case TOP_RIGHT_LOWER: pickup_action_trl(); Serial.write((unsigned int)command); break;
+      case BOTTOM_LEFT_UPPER: pickup_action_blu(); Serial.write((unsigned int)command); break;
+      case BOTTOM_LEFT_LOWER: pickup_action_bll(); Serial.write((unsigned int)command); break;
+      case BOTTOM_RIGHT_UPPER: pickup_action_bru(); Serial.write((unsigned int)command); break;
+      case BOTTOM_RIGHT_LOWER: pickup_action_brl(); Serial.write((unsigned int)command); break;
+      default: Serial.write(99); break;  // junk value
+    }
+  }
+}
--- multi_distance_sensor.ino
+++ multi_distance_sensor.ino
@@ -1,8 +1,8 @@
-#define sensor0 A0 // Sharp IR GP2Y0A41SK0F (4-30cm, analog)
-#define sensor1 A1
-#define sensor2 A2
-#define sensor3 A3
-
+#define sensor1 A0 // Sharp IR GP2Y0A41SK0F (4-30cm, analog)
+#define sensor2 A1
+#define sensor3 A2
+#define sensor4 A3
+// TODO: sensor1 should be top left, 2 should be top right, 3 bottom left
 
 void setup() {
   Serial.begin(9600); // start the serial port
@@ -11,20 +11,20 @@ void setup() {
 void loop() {
   
   // 5v
-  float volt0 = analogRead(sensor0)*0.0048828125;  // value from sensor * (5/1024)
+  float volt0 = analogRead(sensor1)*0.0048828125;  // value from sensor * (5/1024)
   int distance0 = 13*pow(volt0, -1); // worked out from datasheet graph
 
-  float volt1 = analogRead(sensor1)*0.0048828125;  
+  float volt1 = analogRead(sensor2)*0.0048828125;
   int distance1 = 13*pow(volt1, -1); 
 
-  float volt2 = analogRead(sensor2)*0.0048828125;  
+  float volt2 = analogRead(sensor3)*0.0048828125;
   int distance2 = 13*pow(volt2, -1); 
 
-  float volt3 = analogRead(sensor3)*0.0048828125; 
+  float volt3 = analogRead(sensor4)*0.0048828125;
   int distance3 = 13*pow(volt3, -1); 
 
-  // slow down serial port
-  delay(1000); 
+  // slow down serial port, but not too slow
+  delay(200);
 
   Serial.print(distance0);
   Serial.print(" ");
